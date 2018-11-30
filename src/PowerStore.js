02714680bd0797eebbe5d5e8eec6c21a1a957d93
Jeremy Brenner <jeremyjbrenner@gmail.com>
2018-11-30 12:16:54 -0600

    remove unused data structures

--- src/PowerStore.js
+++ src/PowerStore.js
@@ -1,18 +1,6 @@
 import { Store } from 'svelte/store.js';
 import { getOutputStates, setOutputState } from './OctoConnection.js';
 
-const statusMap = {
-  1: 'printerPower',
-  2: 'lightPower',
-  3: 'fanPower'
-}
-
-const idMap = {
-  printerPower: 1,
-  lightPower: 2,
-  fanPower: 3
-}
-
 class PowerStore extends Store {
   keys() {
     return Object.keys(this.get());
