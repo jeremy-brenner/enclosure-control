1c2eddc4e2294ce50dc5e2215d098a14d51837db
Jeremy Brenner <jeremy.brenner@asynchrony.com>
2018-12-04 20:02:20 -0600

    generify temperaturestore to printerstatestore

--- src/stores/PrinterStateStore.js
+++ src/stores/PrinterStateStore.js
@@ -3,10 +3,11 @@ import { getPrinterState } from '../rest/OctoConnection.js';
 
 class TemperatureStore extends UpdatingStore {
   update() {
-    getPrinterState('history=true&limit=1000').then(({temperature})=> {
-      this.set({history: temperature.history});
+    getPrinterState('history=true&limit=1000').then((state)=> {
+      this.set(state);
+      console.log(this.get());
     });
   }
 }
 
-export default new TemperatureStore({history:[]});
+export default new TemperatureStore();
