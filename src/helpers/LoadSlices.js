8692e7805fa07f5af4b219976ffddd61dbdc4d69
Jeremy Brenner <jeremy.brenner@asynchrony.com>
2018-12-15 17:58:38 -0600

    move to npm imports of three js libraries

--- src/helpers/LoadSlices.js
+++ src/helpers/LoadSlices.js
@@ -1,16 +1,21 @@
-const loader = new THREE.STLLoader();
+import * as THREE from 'three';
+import almostStlLoader from 'three-stl-loader';
+
+const STLLoader = almostStlLoader(THREE);
+const loader = new STLLoader();
 
 const LoadSlices = (name) => {
   return Promise.all(fileNames(name).map(loadSlice));
 }
 
 function loadSlice({file,name}) {
-  return new Promise( resolve => 
+  return new Promise( resolve =>  {
     loader.load( file, geometry => {
       geometry.name = name;
       resolve(geometry);
     })
-  );
+   });
+  
 } 
 
 function fileNames(name) {
