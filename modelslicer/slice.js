efa7282c28e99025e108d66f145c0b02331e1bf8
Jeremy Brenner <jeremyjbrenner@gmail.com>
2018-12-16 15:14:54 -0600

    pulled stl parsing/encoding and file writing out of slice.js into cli, added basic primative support

--- modelslicer/slice.js
+++ modelslicer/slice.js
@@ -1,29 +1,14 @@
-const fs = require('fs');
-const stlDeSerializer = require('@jscad/stl-deserializer');
-const stlSerializer = require('@jscad/stl-serializer');
+
 const { CSG } = require('@jscad/csg');
 
-const sliceCount = 100;
+const slice = (inputCsg, sliceCount) => {
 
-const slice = (file,outDir) => {
-
-  if(!file || !file.match(/\.stl$/) ) {
-    console.log("specify an input stl to slice");
-    return;
-  }
-
-  console.log('Slicing...');
-
-  const baseName = file.replace('.stl','').split('/').pop();
-
-  const rawData = fs.readFileSync(file);
-  const inputCsg = stlDeSerializer.deserialize(rawData, undefined, {output: 'csg'}).center([true,true,true]);
-
-  const inputBounds = inputCsg.getBounds();
+  const centeredCsg = inputCsg.center([true,true,true]);
+  const inputBounds = centeredCsg.getBounds();
   const height = inputBounds[1]._z - inputBounds[0]._z;
   const zMulti = 100/height;
 
-  const scaledCsg = inputCsg.scale(zMulti);
+  const scaledCsg = centeredCsg.scale(zMulti);
 
   const bounds = scaledCsg.getBounds();
 
@@ -35,29 +20,17 @@ const slice = (file,outDir) => {
 
   const sliceThickness = box.z/sliceCount;
 
-  const cubeSlice = (i) => CSG.cube({
-    corner2: [bounds[0].x, bounds[0].y, bounds[0].z + sliceThickness*i ],
-    corner1: [bounds[1].x, bounds[1].y, bounds[0].z + sliceThickness*i + sliceThickness]
-  }).intersect(scaledCsg);
+  const cubeSlice = (i) => {
+    console.log(`Slice ${i}`);
+    return CSG.cube({
+      corner2: [bounds[0].x, bounds[0].y, bounds[0].z + sliceThickness*i ],
+      corner1: [bounds[1].x, bounds[1].y, bounds[0].z + sliceThickness*i + sliceThickness]
+    }).intersect(scaledCsg);
+  }
 
-  const promises = numberArray(sliceCount)
-    .map( i => {
-      const filename = `${outDir}/${baseName}-${i.toString().padStart(2, '0')}.stl`;
-      console.log(`Slicing ${filename}`);
-      const stl = stlSerializer.serialize(cubeSlice(i), {binary: false});
-      return saveFile(filename, stl);
-    })
+  const promises = numberArray(sliceCount).map( i => cubeSlice(i) );
   
-  return Promise.all(promises).then( () => console.log('Done!'));
-}
-
-function saveFile(filename, data) {
-  console.log(`Saving ${filename}`);
-  return new Promise( (resolve,reject) => {
-    fs.writeFile( filename, data, (err) => err ? reject(err): resolve());
-  })
-  .then(() => console.log(`Saved ${filename}`))
-  .catch((err) => console.error(`Problem saving ${filename}: ${err}`));
+  return Promise.all(promises);
 }
 
 function numberArray(count) {
