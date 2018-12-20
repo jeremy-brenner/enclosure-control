9455aaa1d25163ff3714ed905b9c463c13c491be
Jeremy Brenner <jeremyjbrenner@gmail.com>
2018-12-19 21:34:19 -0600

    output binary

--- modelslicer/slice_cli.js
+++ modelslicer/slice_cli.js
@@ -1,4 +1,4 @@
-const slice = require('./slice');
+const slicer = require('./slice');
 const fs = require('fs');
 const stlDeSerializer = require('@jscad/stl-deserializer');
 const stlSerializer = require('@jscad/stl-serializer');
@@ -7,39 +7,45 @@ const { CSG } = require('@jscad/csg');
 const file = process.argv[2];
 const outDir = process.argv[3];
 
-if(!file) {
+if(!file || !file.match(/\.stl$/)) {
   console.log("specify an input stl to slice");
   return;
 }
 
-let baseName, inputCsg;
-
-if( file.match(/\.stl$/) ) {
-  baseName = file.replace('.stl','').split('/').pop();
-  const rawData = fs.readFileSync(file);
-  inputCsg = stlDeSerializer.deserialize(rawData, undefined, {output: 'csg'});
-}else{
-  baseName = file
-  inputCsg = CSG[file]();
-}
+const baseName = file.replace('.stl','').split('/').pop();
+const rawData = fs.readFileSync(file);
+const inputCsg = stlDeSerializer.deserialize(rawData, undefined, {output: 'csg'});
 
 console.log('Slicing...');
 
-slice(inputCsg,100)
-  .then( slices => slices.map( (slice,i) => {
+const slice = slicer(inputCsg,100);
+
+numbers(100)
+  .reduce( (p,i) => {
+    return p.then( () => {
       const filename = `${outDir}/${baseName}-${i.toString().padStart(2, '0')}.stl`;
-      const stl = stlSerializer.serialize(slice, {binary: false});
-      return saveFile(filename, stl);
-    }))
-  .then( p => Promise.all(p))
+      const stl = stlSerializer.serialize( slice(i), {binary: true});
+      return saveFile(filename, stl)
+    });
+  }, Promise.resolve())
   .then(() => console.log('Done!'));
 
 
 function saveFile(filename, data) {
     console.log(`Saving ${filename}`);
     return new Promise( (resolve,reject) => {
-      fs.writeFile( filename, data, (err) => err ? reject(err): resolve());
+      const stream = fs.createWriteStream(filename);
+      data.forEach( ab => {
+        stream.write(Buffer.from(ab));
+      })
+      stream.end();
+      stream.on('finish', () => resolve() );
     })
     .catch((err) => console.error(`Problem saving ${filename}: ${err}`));
   }
   
+
+
+function numbers(count) {
+  return [...Array(count).keys()];
+}
